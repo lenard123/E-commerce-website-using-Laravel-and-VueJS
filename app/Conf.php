2362c525fcc67dda2fc1accd27a1333ad356b13e
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2018-10-30 15:46:36 +0800

    v2.2.1 Config class created
    For constant value
    
    Signed-off-by: Lenard Mangay-ayam <lenard.mangayayam@gmail.com>

---
+++ app/Conf.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class Conf
+{
+    const ROLE_CUSTOMER = 0;
+    const ROLE_SUBADMIN = 1;
+    const ROLE_ADMIN = 2;
+
+    const IMAGE_DEFAULT = 'images/default.jpg';
+    const IMAGE_USER = 'images/user.jpg';
+
+    const ORDER_NEW = 0;
+    const ORDER_COMPLETED = 1;
+    const ORDER_CANCELLED = 2;
+}
